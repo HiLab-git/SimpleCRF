53adbbd1af3034c51428b43362c1bddf8bde16c6
Guotai Wang <wguotai@gmail.com>
2019-07-02 20:24:42 +0800

    remove build files

--- crf_max_flow_python/setup.py
+++ crf_max_flow_python/setup.py
@@ -1,14 +1,15 @@
 from distutils.core import setup
 from distutils.extension import Extension
+import numpy
 
 package_name = 'crf_max_flow'
 module_name1 = 'max_flow'
 module_name2 = 'max_flow3d'
 module1 = Extension(module_name1,
-                    include_dirs = ['/Users/guotaiwang/anaconda/lib/python2.7/site-packages/numpy/core/include'],
+                    include_dirs = [numpy.get_include()],
                     sources = ['max_flow.cpp', 'maxflow-v3.0/graph.cpp', 'maxflow-v3.0/maxflow.cpp'])
 module2 = Extension(module_name2,
-                    include_dirs = ['/Users/guotaiwang/anaconda/lib/python2.7/site-packages/numpy/core/include'],
+                    include_dirs = [numpy.get_include()],
                     sources = ['max_flow3d.cpp', 'maxflow-v3.0/graph.cpp', 'maxflow-v3.0/maxflow.cpp'])
 setup(name=package_name,
       ext_modules = [module1, module2])
